83d9358ad4062d64f01d180ff7ad64906e1ad912
Sofus Lund <sofuslund.dev@gmail.com>
2022-06-25 17:17:47 +0200

    Make logic for fetching countries by search

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,28 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, computed, type Ref } from "vue";
 
-<template></template>
+const inputVal = ref("green");
+let countries = ref([]);
 
-<style></style>
+let anyCountries = ref(false);
+
+async function updateCountries() {
+    try {
+        const response = await fetch(`https://restcountries.com/v3.1/name/${inputVal.value}`);
+        let data = await response.json();
+
+        anyCountries.value = response.ok;
+        countries.value = data.map((elm: {name: {official: string}}) => elm.name.official);
+
+    } catch (e) {
+        alert('Error: ' + e);
+    }
+};
+
+</script>
+
+<template>
+</template>
+
+<style>
+</style>
