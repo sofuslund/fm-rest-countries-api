838e31692d140e4c2dd269b1719a41da2842aeed
Sofus Lund <sofuslund.dev@gmail.com>
2022-06-25 13:23:28 +0200

    Add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,14 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      'oxford-blue': '#2b3945',
+      'ebony-clay': '#202c37',
+      woodsmoke: '#111517',
+      gray: '#858585',
+      alabaster: '#fafafa',
+      white: '#ffffff',
+    },
     extend: {},
   },
   plugins: [],
