1d248a2473d0bc8f6ffe1551a0b0f763da7ca94c
Sofus Lund <sofuslund.dev@gmail.com>
2022-06-26 13:31:02 +0200

    Add font awesome

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,21 @@
 /* eslint-env node */
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
-  theme: {
-    colors: {
-      'oxford-blue': '#2b3945',
-      'ebony-clay': '#202c37',
-      woodsmoke: '#111517',
-      gray: '#858585',
-      alabaster: '#fafafa',
-      white: '#ffffff',
+    content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+    theme: {
+        colors: {
+            "oxford-blue": "#2b3945",
+            "ebony-clay": "#202c37",
+            woodsmoke: "#111517",
+            gray: "#858585",
+            alabaster: "#fafafa",
+            white: "#ffffff",
+        },
+        fontFamily: {
+            "sans-serif": "Nunito Sans",
+            "fa-font-solid": "var(--fa-font-solid)"
+        },
+        extend: {},
     },
-    fontFamily: {
-      'sans-serif': 'Nunito Sans'
-    },
-    extend: {},
-  },
-  plugins: [],
+    plugins: [],
 };
