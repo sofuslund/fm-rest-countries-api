2e8766f1808c52131c23778f26375cf31ff141c1
Sofus Lund <sofuslund.dev@gmail.com>
2022-06-26 18:11:49 +0200

    Fix SearchBar icon placement and outline

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -8,8 +8,10 @@ export default {
 </script>
 
 <template>
-    <div class="relative p-4">
-        <i class="fa-solid fa-magnifying-glass absolute left-10 -translate-x-1/2 top-1/2 -translate-y-1/2 opacity-40"></i>
-        <input v-bind="$attrs" class="w-full py-3 pl-20 shadow-md rounded-md placeholder-gray placeholder:font-light" type="text" placeholder="Search for a country..." />
+    <div class="p-4">
+        <div class="relative">
+            <i class="fa-solid fa-magnifying-glass absolute left-10 -translate-x-1/2 top-1/2 -translate-y-1/2 opacity-40"></i>
+            <input v-bind="$attrs" class="focus:outline-none w-full py-3 pl-20 shadow-md rounded-md placeholder-gray placeholder:font-light" type="text" placeholder="Search for a country..." />
+        </div>
     </div>
 </template>
