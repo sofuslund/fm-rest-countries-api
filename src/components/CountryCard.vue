9dd89a52c3b849255442221af2ed3011e7e99798
Sofus Lund <sofuslund.dev@gmail.com>
2022-06-28 18:59:54 +0200

    Fix CountryCard logic

--- src/components/CountryCard.vue
+++ src/components/CountryCard.vue
@@ -1,16 +1,19 @@
 <script setup lang="ts">
 import { computed } from '@vue/reactivity';
 
-const props = defineProps<{ country: {name: string, population: number, region: string, capital: string} }>();
+const props = defineProps<{ country: Country}>();
 
 const capital = computed(() => {
+    // The country might not have a capital 
+    if(!props.country.hasOwnProperty("capital"))
+        return "None";
     // The country capital is an array as it may have more capitals. In that case the capitals should be displayed with commas in between
-    return Object.values(props.country.capital).join(", ");
+    return props.country.capital.join(", ");
 });
 </script>
 <template>
     <div>
-        <h1>{{country.name}}</h1>
+        <h1>{{country.name.common}}</h1>
         <p><strong>Population: </strong>{{country.population}}</p>
         <p><strong>Region: </strong>{{country.region}}</p>
         <p><strong>Capital: </strong>{{capital}}</p>
