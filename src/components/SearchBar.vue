e7d528c1ec8d473832b42cf3dab3f9748356c2e1
Sofus Lund <sofuslund.dev@gmail.com>
2022-06-28 19:15:11 +0200

    Change space on SearchBar sides

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -8,7 +8,7 @@ export default {
 </script>
 
 <template>
-    <div class="p-4">
+    <div class="px-4 my-8">
         <div class="relative">
             <i class="fa-solid fa-magnifying-glass absolute left-10 -translate-x-1/2 top-1/2 -translate-y-1/2 opacity-40"></i>
             <input v-bind="$attrs" class="bg-white focus:outline-none w-full py-3 pl-20 shadow-md rounded-md placeholder-gray placeholder:font-light" type="text" placeholder="Search for a country..." />
