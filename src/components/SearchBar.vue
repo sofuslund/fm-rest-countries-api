53d070680d9b1055dba6f4f602347a7950449d7c
Sofus Lund <sofuslund.dev@gmail.com>
2022-06-30 16:23:08 +0200

    Change attribute inheritance

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,17 +1,29 @@
-<script>
+<script lang="ts">
 export default {
     inheritAttrs: false
 }
 </script>
 <script setup lang="ts">
+import { computed } from "@vue/reactivity";
+import { useAttrs } from "vue";
 
+const attrs = useAttrs();
+
+const attrsExceptClass = computed(() => {
+    let returnAttrs: {[key: string]: any} = {};
+    for(let key in attrs) {
+        if(key !== "class")
+            returnAttrs[key] = attrs[key];
+    }
+    return returnAttrs;
+});
 </script>
 
 <template>
-    <div class="mx-4 my-8">
+    <div :class="$attrs['class']">
         <div class="relative">
             <i class="fa-solid fa-magnifying-glass absolute left-10 -translate-x-1/2 top-1/2 -translate-y-1/2 opacity-40"></i>
-            <input v-bind="$attrs" class="bg-white focus:outline-none w-full py-3 pl-20 shadow-md rounded-md placeholder-gray placeholder:font-light" type="text" placeholder="Search for a country..." />
+            <input v-bind="attrsExceptClass" class="bg-white focus:outline-none w-full py-3 pl-20 shadow-md rounded-md placeholder-gray placeholder:font-light" type="text" placeholder="Search for a country..." />
         </div>
     </div>
 </template>
