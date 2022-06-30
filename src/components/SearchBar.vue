abfcc1a5877b906ff6621d4df1146ab40bfa509f
Sofus Lund <sofuslund.dev@gmail.com>
2022-06-30 16:36:57 +0200

    Make placeholder inherited

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -7,6 +7,8 @@ export default {
 import { computed } from "@vue/reactivity";
 import { useAttrs } from "vue";
 
+const props = defineProps<{placeholder: string}>();
+
 const attrs = useAttrs();
 
 const attrsExceptClass = computed(() => {
@@ -23,7 +25,7 @@ const attrsExceptClass = computed(() => {
     <div :class="$attrs['class']">
         <div class="relative">
             <i class="fa-solid fa-magnifying-glass absolute left-10 -translate-x-1/2 top-1/2 -translate-y-1/2 opacity-40"></i>
-            <input v-bind="attrsExceptClass" class="bg-white focus:outline-none w-full py-3 pl-20 shadow-md rounded-md placeholder-gray placeholder:font-light" type="text" placeholder="Search for a country..." />
+            <input v-bind="attrsExceptClass" class="bg-white focus:outline-none w-full py-3 pl-20 shadow-md rounded-md placeholder-gray placeholder:font-light" type="text" />
         </div>
     </div>
 </template>
