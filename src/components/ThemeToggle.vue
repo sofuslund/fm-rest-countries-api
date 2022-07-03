497212279dae297628fb29c852e6229f5bc2760a
Sofus Lund <sofuslund.dev@gmail.com>
2022-07-03 12:50:40 +0200

    Make ThemeToggle component

---
+++ src/components/ThemeToggle.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const emit = defineEmits<{(e: "newTheme", theme: "dark" | "light"): void}>();
+
+let theme = ref<"dark" | "light">("light");
+
+function switchMode() {
+    if(theme.value === "dark") {
+        theme.value = "light";
+    } else {
+        theme.value = "dark";
+    }
+    emit("newTheme", theme.value);
+}
+
+</script>
+<template>
+<div @click="switchMode" class="rounded-full hover:bg-alabaster px-2 text-woodsmoke font-semibold select-none">
+    <div v-if="theme === 'dark'">
+        <i class="fa-regular fa-moon mr-3"></i>
+        <span>Dark Mode</span>
+    </div>
+    <div v-else>
+        <i class="fa-regular fa-sun mr-3"></i>
+        <span>Light Mode</span>
+    </div>
+</div>
+</template>
