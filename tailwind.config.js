6a0ab0970f2e08387ebab9053c0e20e41d92fff3
Sofus Lund <sofuslund.dev@gmail.com>
2022-07-08 18:57:26 +0200

    Fix dark theme shadow color

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,7 +19,7 @@ module.exports = {
             boxShadow: {
                 "md-dark": "0px 0px .50rem hsl(360, 0%, 10%)",
                 around: "0px 0px .75rem hsl(360, 0%, 90%)",
-                "around-dark": "0px 0px .75rem hsl(360, 0%, 20%)"
+                "around-dark": "0px 0px .75rem hsl(360, 0%, 12%)"
             }
         },
     },
