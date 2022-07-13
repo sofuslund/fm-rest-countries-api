8240fbbcc5df6939651aae344710bcffcc483e15
Sofus Lund <sofuslund.dev@gmail.com>
2022-07-13 19:32:23 +0200

    Make theme persistent across browser reloads

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,16 @@
 <script setup lang="ts">
-import { ref } from "vue";
+import { ref, onMounted } from "vue";
 
 import ThemeToggle from "@/components/ThemeToggle.vue";
 
-let theme = ref<Theme>("light");
+let theme = ref<Theme>(window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light");
+
+onMounted(() => {
+    let savedTheme = localStorage.getItem("theme");
+    if(savedTheme)
+        theme.value = savedTheme as Theme;
+});
+
 </script>
 
 <template>
@@ -12,7 +19,7 @@ let theme = ref<Theme>("light");
             <RouterLink to="/home">
                 <div class="py-7 px-5 bg-white dark:bg-oxford-blue shadow-md dark:shadow-md-dark relative z-10">
                     <h1 class="font-extrabold inline dark:text-white">Where in the world?</h1>
-                    <ThemeToggle v-model="theme" class="float-right"></ThemeToggle>
+                    <ThemeToggle v-model="theme" save-item="theme" class="float-right"></ThemeToggle>
                 </div>
             </RouterLink>
             
--- src/components/ThemeToggle.vue
+++ src/components/ThemeToggle.vue
@@ -1,13 +1,16 @@
 <script setup lang="ts">
-import { ref, watch } from "vue";
+import { ref, watch, onMounted } from "vue";
 
 const emit = defineEmits<{(e: "update:modelValue", val: Theme): void}>();
-const props = defineProps<{modelValue: Theme}>();
+const props = defineProps<{modelValue: Theme, saveItem?: string}>();
 
 let theme = ref<Theme>(props.modelValue);
 
 watch(() => props.modelValue, val => {
     theme.value = val;
+    if(props.saveItem) {
+        localStorage.setItem(props.saveItem, val); 
+    }
 });
 
 function switchTheme() {
@@ -16,13 +19,16 @@ function switchTheme() {
     } else {
         theme.value = "dark";
     }
+    
+    if(props.saveItem)
+        localStorage.setItem(props.saveItem, theme.value);
     emit("update:modelValue", theme.value);
 }
 
 </script>
 <template>
 <div @click="switchTheme" class="rounded-full hover:bg-alabaster dark:hover:bg-ebony-clay px-2 text-woodsmoke dark:text-white font-semibold select-none">
-    <div v-if="theme === 'light'">
+    <div v-if="theme === 'dark'">
         <i class="fa-regular fa-moon mr-3"></i>
         <span>Dark Mode</span>
     </div>
